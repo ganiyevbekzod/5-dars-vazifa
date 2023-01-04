c69ee3994d45fd3b40bd9c4e4884bfa4cbd2ad4c
ganiyevbekzod <115441938+ganiyevbekzod@users.noreply.github.com>
2023-01-04 23:20:28 +0500

    to'liq

--- js/main.js
+++ js/main.js
@@ -5,18 +5,22 @@ const elList=document.querySelector(".js-list");
 const elDeleteBtn=document.querySelector(".js-delete-btn");
 const newEditBtn=document.querySelector(".js-edit-btn");
 const ElAllcount=document.querySelector(".js-Allcount");
-const ElComplateSpan=document.querySelector(".js-Complate");
-const ElUncompleted=document.querySelector(".js-uncomplate");
+const ElComplateSpan=document.querySelector(".js-Complatecount");
+const ElUncompleted=document.querySelector(".js-uncomplatecount");
 const ElDarkmode=document.querySelector(".js-dark")
-const todos=[];
+const elAllBtns=document.querySelector(".js-btns")
+const localData=JSON.parse(window.localStorage.getItem("todos"))
+const todos=localData ? localData:[];
 
 const renderTodo=(array,node) =>{
-    elList.innerHTML="";
-    ElAllcount.textContent=array.length;
-    ElComplateSpan.textContent=array.filter((item)=>item.isCompleted).length;
-    ElUncompleted.textContent=array.filter((item)=>!item.isCompleted).length;
+        // TODOlarni obyektdan stringga utkazish
+        window.localStorage.setItem("todos",JSON.stringify(todos))
+    ElAllcount.textContent=todos.length;
+    ElComplateSpan.textContent=todos.filter((item)=>item.isCompleted).length;
+    ElUncompleted.textContent=todos.filter((item)=>!item.isCompleted).length;
+    node.innerHTML="";
     
-    array.forEach(item => {array
+    array.forEach((item) => {
        const newItem=document.createElement("li") ;
        const newSpan=document.createElement("span")
        const newInput=document.createElement("input")
@@ -30,47 +34,37 @@ const renderTodo=(array,node) =>{
        newSpan.textContent=item.text;
        newInput.type="checkbox";
        newDeleteButton.textContent="Delete";
-       newEditButton.textContent="Edit";
-
-
+       newEditButton.textContent="Edit";    
+       newDeleteButton.dataset.todoId=item.id;
+       newEditButton.dataset.todoId=item.id;
+       newInput.dataset.todoId=item.id;
+       newItem.appendChild(newInput);
+       newItem.appendChild(newSpan);
+       newItem.appendChild(newEditButton);
+       newItem.appendChild(newDeleteButton)
+       node.appendChild(newItem);
        if(item.isCompleted){
         newInput.checked=true;
         newSpan.style.textDecoration="line-through"
         newSpan.style.color="red";
        }
-
-
-
-       newDeleteButton.dataset.todoId=item.id;
-       newEditButton.dataset.todoId=item.id;
-       newInput.dataset.todoId=item.id;
-
-
-       newItem.appendChild(newInput);
-       newItem.appendChild(newSpan);
-       newItem.appendChild(newEditButton);
-       newItem.appendChild(newDeleteButton)
-      node.appendChild(newItem);
-
     });
 };
+renderTodo(todos,elList)
 elForm.addEventListener("submit",function(evt){
     evt.preventDefault();
 
     const newTodo={
-        id:todos.length+1,
+        id:todos.length? todos[todos.length-1].id+1 :1,
         text:elInput.value,
         isCompleted:false,
     }
-
-
     todos.push(newTodo);
     elInput.value="";
     renderTodo(todos,elList)
 
+
 })
-
-
 
 elList.addEventListener("click",function(evt){
     if((evt.target.matches(".js-delete-btn"))){
@@ -79,10 +73,10 @@ elList.addEventListener("click",function(evt){
     todos.splice(findedIndex,1);
     renderTodo(todos,elList);
 };
-if(evt.target.matches(".js-edit-btn")){
+    if(evt.target.matches(".js-edit-btn")){
     const todoId=evt.target.dataset.todoId;
     const findedItem=todos.find((item) => item.id==todoId);
-    const NewText=prompt("ISMINGIZNI TAHRIRLANG  ",findedItem.text)
+    const NewText=prompt("ISMINGIZNI TAHRIRLANG",findedItem.text)
     findedItem.text=NewText
     renderTodo(todos,elList)
 }
@@ -93,6 +87,21 @@ if(evt.target.matches(".js-check")){
     renderTodo(todos,elList)
     }
 })
+
+elAllBtns.addEventListener("click",function(evt){
+    if(evt.target.matches(".js-all")){
+        renderTodo(todos,elList)
+    }
+    if(evt.target.matches(".js-complate")){
+        const filteredTodos=todos.filter((item)=>item.isCompleted);
+        renderTodo(filteredTodos,elList)
+    }
+    if(evt.target.matches(".js-uncomplate")){
+        const filteredTodos=todos.filter((item)=>!item.isCompleted);
+        renderTodo(filteredTodos,elList)
+    }
+})
+
 // darkmode
 let theme=false
 ElDarkmode.addEventListener("click",function (){
@@ -110,6 +119,7 @@ function changeTheme() {
     }
     changeTheme()
 }
+
 
 
 
